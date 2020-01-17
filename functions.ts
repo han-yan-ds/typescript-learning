b6073629407091677cc907f725d0378631b6c95d
han-yan-ds <hanprogramming@gmail.com>
2020-01-17 16:34:08 -0700

    Modified function example to be better/stricter

--- functions.ts
+++ functions.ts
@@ -2,7 +2,7 @@ function print(n1: number): void {
   return; // +n1 + n2;
 }
 
-let combineValues: Function;
+let combineValues: (x: number) => void; // I COULD write combineValues: Function, but that doesn't let me control the parameters or the return value
 combineValues = print;
 
 function hasCb(arg1: number, cb: (x: number) => number): number {
